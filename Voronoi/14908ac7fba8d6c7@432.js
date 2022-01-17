f909b055a3c46800e7154ed34b68bd2048720a75
Daniel <97815389+NecessaryForm@users.noreply.github.com>
2022-01-17 02:56:41 -0500

    Update 14908ac7fba8d6c7@432.js

--- Voronoi/14908ac7fba8d6c7@432.js
+++ Voronoi/14908ac7fba8d6c7@432.js
@@ -3,7 +3,7 @@ export default function define(runtime, observer) {
   const fileAttachments = new Map([["14959f050311f400368624031a7b9e4285f35c65ca4022f618f9250d7163ef4b0a0582de20f7d9790ed76b3442b4a77ebb96b86f641c1d8466f6544325144aed@3.png",new URL("./files/fa6c91acc020c3b919dd098c23a75d3b64a74ea978c1dfba0263e0d848581f7d4ba7cbe4a8c73b7efd756cd00b7a3a86a3b2723704145eb507c6d139e1a7829e",import.meta.url)]]);
   main.builtin("FileAttachment", runtime.fileAttachments(name => fileAttachments.get(name)));
   main.variable(observer()).define(["md"], function(md){return(
-md`# Voronoi Stippling
+md`# Voronoi Stipplings
 
 This notebook applies a weighted variant of [Lloyd’s algorithm](/@mbostock/lloyds-algorithm) to implement stippling. Points are initially positioned randomly using rejection sampling, then at each iteration, the Voronoi cell centroids are weighted by the lightness of the contained pixels.
 
